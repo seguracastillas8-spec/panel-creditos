4cb6f733278b62512008107fa20e82890307c803
seguracastillas8-spec <seguracastillas8@gmail.com>
2026-01-04 15:07:30 -0500

    Update auth.middleware.js

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,1 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch {
+    res.sendStatus(403);
+  }
+};
 
