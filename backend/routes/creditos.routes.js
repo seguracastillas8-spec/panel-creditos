59105a97eae815d9dddb2e4876aaaec4d2faf718
seguracastillas8-spec <seguracastillas8@gmail.com>
2026-01-04 15:04:10 -0500

    Update creditos.routes.js

--- backend/routes/creditos.routes.js
+++ backend/routes/creditos.routes.js
@@ -1,1 +1,18 @@
+import express from 'express';
+import {
+  crearCredito,
+  listarCreditos,
+  borrarCredito
+} from '../controllers/creditos.controller.js';
+
+import auth from '../middlewares/auth.middleware.js';
+import roles from '../middlewares/roles.middleware.js';
+
+const router = express.Router();
+
+router.post('/', auth, crearCredito);
+router.get('/', auth, listarCreditos);
+router.delete('/:id', auth, roles('ADMIN_GENERAL'), borrarCredito);
+
+export default router;
 
