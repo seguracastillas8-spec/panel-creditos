d93212a04813a74bd96d8580f6fcdd5fefb15291
seguracastillas8-spec <seguracastillas8@gmail.com>
2026-01-04 15:11:59 -0500

    Update auth.js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,1 +1,14 @@
+async function login() {
+  const res = await fetch('/api/auth/login', {
+    method: 'POST',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      usuario: user.value,
+      password: pass.value
+    })
+  });
+  const data = await res.json();
+  localStorage.token = data.token;
+  location.href = 'dashboard.html';
+}
 
