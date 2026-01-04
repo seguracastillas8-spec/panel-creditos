c6a8a20261c49ed4402b4697ba253dec09622329
seguracastillas8-spec <seguracastillas8@gmail.com>
2026-01-04 15:04:55 -0500

    Update usuarios.controller.js

--- backend/controllers/usuarios.controller.js
+++ backend/controllers/usuarios.controller.js
@@ -1,1 +1,9 @@
+import pool from '../db.js';
+
+export const listarUsuarios = async (req, res) => {
+  const result = await pool.query(
+    'SELECT usuario, rol FROM usuarios'
+  );
+  res.json(result.rows);
+};
 
