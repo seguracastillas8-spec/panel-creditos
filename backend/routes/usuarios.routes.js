f71b14dd8fd464b51a86848d004db3e431862864
seguracastillas8-spec <seguracastillas8@gmail.com>
2026-01-04 15:03:11 -0500

    Update usuarios.routes.js

--- backend/routes/usuarios.routes.js
+++ backend/routes/usuarios.routes.js
@@ -1,1 +1,10 @@
+import express from 'express';
+import { listarUsuarios } from '../controllers/usuarios.controller.js';
+import auth from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+router.get('/', auth, listarUsuarios);
+
+export default router;
 
